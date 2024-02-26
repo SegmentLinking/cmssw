02a13456805085be021af8d214a453727ed6827b
Andres Rios Tascon <ariostas@gmail.com>
2024-02-26 07:25:03 -0800

    Added simpler LSTCore alternative

---
+++ RecoTracker/LSTCoreAlt/interface/ModuleConnectionMap.h
@@ -0,0 +1,35 @@
+#ifndef ModuleConnectionMap_h
+#define ModuleConnectionMap_h
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <sstream>
+#include <algorithm>
+
+namespace SDL {
+  class ModuleConnectionMap {
+  private:
+    std::map<unsigned int, std::vector<unsigned int>> moduleConnections_;
+
+  public:
+    ModuleConnectionMap();
+    ModuleConnectionMap(std::string filename);
+    ~ModuleConnectionMap();
+
+    void load(std::string);
+    void add(std::string);
+    void print();
+
+    const std::vector<unsigned int>& getConnectedModuleDetIds(unsigned int detid) const;
+    int size() const;
+  };
+
+  extern ModuleConnectionMap moduleConnectionMap;
+  extern std::vector<ModuleConnectionMap> moduleConnectionMap_pLStoLayer;
+  extern std::vector<ModuleConnectionMap> moduleConnectionMap_pLStoLayer_pos;
+  extern std::vector<ModuleConnectionMap> moduleConnectionMap_pLStoLayer_neg;
+}  // namespace SDL
+
+#endif
