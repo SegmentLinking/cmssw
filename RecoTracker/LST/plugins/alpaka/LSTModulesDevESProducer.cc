ccace1ec3a5e08d55deeae2a2b8483c5ed46be79
Manos Vourliotis <emmanouil.vourliotis@gmail.com>
2024-05-13 08:05:37 -0700

    Code checks and format

--- RecoTracker/LST/plugins/alpaka/LSTModulesDevESProducer.cc
+++ RecoTracker/LST/plugins/alpaka/LSTModulesDevESProducer.cc
@@ -15,41 +15,39 @@ namespace cms::alpakatools {
   template <>
   struct CopyToDevice<SDL::modulesBuffer<alpaka_common::DevHost>> {
     template <typename TQueue>
-    static auto copyAsync(TQueue& queue, SDL::modulesBuffer<alpaka_common::DevHost> const& srcData) {
+    static auto copyAsync(TQueue &queue, SDL::modulesBuffer<alpaka_common::DevHost> const &srcData) {
       return SDL::modulesBuffer<alpaka::Dev<TQueue>>(queue, srcData);
     }
   };
-} //namespace cms::alpakatools
+}  //namespace cms::alpakatools
 namespace ALPAKA_ACCELERATOR_NAMESPACE {
 
-class LSTModulesDevESProducer : public ESProducer {
-public:
-  LSTModulesDevESProducer(const edm::ParameterSet &iConfig);
+  class LSTModulesDevESProducer : public ESProducer {
+  public:
+    LSTModulesDevESProducer(const edm::ParameterSet &iConfig);
 
-  static void fillDescriptions(edm::ConfigurationDescriptions &descriptions);
+    static void fillDescriptions(edm::ConfigurationDescriptions &descriptions);
 
-  std::optional<SDL::modulesBuffer<alpaka_common::DevHost>> produce(const TrackerRecoGeometryRecord &iRecord);
+    std::optional<SDL::modulesBuffer<alpaka_common::DevHost>> produce(const TrackerRecoGeometryRecord &iRecord);
+  };
 
-};
+  LSTModulesDevESProducer::LSTModulesDevESProducer(const edm::ParameterSet &iConfig) : ESProducer(iConfig) {
+    setWhatProduced(this, iConfig.getParameter<std::string>("ComponentName"));
+  }
 
-LSTModulesDevESProducer::LSTModulesDevESProducer(const edm::ParameterSet &iConfig)
-  : ESProducer(iConfig)
-{
-  setWhatProduced(this, iConfig.getParameter<std::string>("ComponentName"));
-}
+  void LSTModulesDevESProducer::fillDescriptions(edm::ConfigurationDescriptions &descriptions) {
+    edm::ParameterSetDescription desc;
+    desc.add<std::string>("ComponentName", "")->setComment("Product label");
+    descriptions.addWithDefaultLabel(desc);
+  }
 
-void LSTModulesDevESProducer::fillDescriptions(edm::ConfigurationDescriptions &descriptions) {
-  edm::ParameterSetDescription desc;
-  desc.add<std::string>("ComponentName", "")->setComment("Product label");
-  descriptions.addWithDefaultLabel(desc);
-}
-
-  std::optional<SDL::modulesBuffer<alpaka_common::DevHost>> LSTModulesDevESProducer::produce(const TrackerRecoGeometryRecord &iRecord) {
+  std::optional<SDL::modulesBuffer<alpaka_common::DevHost>> LSTModulesDevESProducer::produce(
+      const TrackerRecoGeometryRecord &iRecord) {
     SDL::modulesBuffer<alpaka_common::DevHost> modules(cms::alpakatools::host());
     alpaka::QueueCpuBlocking queue(cms::alpakatools::host());
     SDL::LST<SDL::Acc>::loadAndFillES(queue, &modules);
     return modules;
-}
+  }
 
 }  // namespace ALPAKA_ACCELERATOR_NAMESPACE
 
