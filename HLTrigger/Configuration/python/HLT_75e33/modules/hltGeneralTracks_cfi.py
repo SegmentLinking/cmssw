c524c36fa9cf3bbdce5b1b4c6f7cd7bcd6b72906
Manos Vourliotis <emmanouil.vourliotis@gmail.com>
2024-09-18 08:44:29 -0700

    Make SiPhase2RecHits used for LST consistent with the HLT SiPhase2Clusters

--- HLTrigger/Configuration/python/HLT_75e33/modules/hltGeneralTracks_cfi.py
+++ HLTrigger/Configuration/python/HLT_75e33/modules/hltGeneralTracks_cfi.py
@@ -26,10 +26,10 @@ hltGeneralTracks = cms.EDProducer("TrackListMerger",
 )
 
 _hltGeneralTracksLST = hltGeneralTracks.clone(
-    TrackProducers = cms.VInputTag("hltInitialStepTrackSelectionHighPuritypTTCLST","hltInitialStepTrackSelectionHighPuritypLSTCLST","hltInitialStepTracksT5TCLST", "hltHighPtTripletStepTrackSelectionHighPurity"),
+    TrackProducers = cms.VInputTag("hltInitialStepTrackSelectionHighPuritypTTCLST", "hltInitialStepTrackSelectionHighPuritypLSTCLST", "hltInitialStepTracksT5TCLST", "hltHighPtTripletStepTrackSelectionHighPurity"),
     hasSelector = cms.vint32(0,0,0,0),
     indivShareFrac = cms.vdouble(0.1,0.1,0.1,0.1),
-    selectedTrackQuals = cms.VInputTag(cms.InputTag("hltInitialStepTrackSelectionHighPuritypTTCLST"),cms.InputTag("hltInitialStepTrackSelectionHighPuritypLSTCLST"),cms.InputTag("hltInitialStepTracksT5TCLST"),cms.InputTag("hltHighPtTripletStepTrackSelectionHighPurity")),
+    selectedTrackQuals = cms.VInputTag(cms.InputTag("hltInitialStepTrackSelectionHighPuritypTTCLST"), cms.InputTag("hltInitialStepTrackSelectionHighPuritypLSTCLST"), cms.InputTag("hltInitialStepTracksT5TCLST"), cms.InputTag("hltHighPtTripletStepTrackSelectionHighPurity")),
     setsToMerge = cms.VPSet(cms.PSet(
         pQual = cms.bool(True),
         tLists = cms.vint32(0,1,2,3)
@@ -40,10 +40,10 @@ from Configuration.ProcessModifiers.trackingLST_cff import trackingLST
 trackingLST.toReplaceWith(hltGeneralTracks, _hltGeneralTracksLST)
 
 _hltGeneralTracksLSTSeeding = hltGeneralTracks.clone(
-            TrackProducers = cms.VInputTag("hltInitialStepTrackSelectionHighPuritypTTCLST","hltInitialStepTracksT5TCLST","hltHighPtTripletStepTrackSelectionHighPuritypLSTCLST"),
+            TrackProducers = cms.VInputTag("hltInitialStepTrackSelectionHighPuritypTTCLST", "hltInitialStepTracksT5TCLST", "hltHighPtTripletStepTrackSelectionHighPuritypLSTCLST"),
             hasSelector = cms.vint32(0,0,0),
             indivShareFrac = cms.vdouble(0.1,0.1,0.1),
-            selectedTrackQuals = cms.VInputTag(cms.InputTag("hltInitialStepTrackSelectionHighPuritypTTCLST"),cms.InputTag("hltInitialStepTracksT5TCLST"),cms.InputTag("hltHighPtTripletStepTrackSelectionHighPuritypLSTCLST")),
+            selectedTrackQuals = cms.VInputTag(cms.InputTag("hltInitialStepTrackSelectionHighPuritypTTCLST"), cms.InputTag("hltInitialStepTracksT5TCLST"), cms.InputTag("hltHighPtTripletStepTrackSelectionHighPuritypLSTCLST")),
             setsToMerge = cms.VPSet(cms.PSet(
                pQual = cms.bool(True),
                tLists = cms.vint32(0,1,2)
--- HLTrigger/Configuration/python/HLT_75e33/modules/hltSiPhase2RecHits_cfi.py
+++ HLTrigger/Configuration/python/HLT_75e33/modules/hltSiPhase2RecHits_cfi.py
@@ -1,5 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
 from RecoLocalTracker.Phase2TrackerRecHits.Phase2TrackerRecHits_cfi import siPhase2RecHits as _siPhase2RecHits
-hltSiPhase2RecHits = _siPhase2RecHits.clone()
+hltSiPhase2RecHits = _siPhase2RecHits.clone( src = cms.InputTag("hltSiPhase2Clusters") )
 
