d55b05bff92b8c6f90aec8a4657d15502f393574
Andres Rios Tascon <ariostas@gmail.com>
2025-03-27 08:16:43 -0700

    Tried to add dict for device classes

--- RecoTracker/LSTCore/src/alpaka/LSTEvent.dev.cc
+++ RecoTracker/LSTCore/src/alpaka/LSTEvent.dev.cc
@@ -80,7 +80,7 @@ void LSTEvent::resetEventSync() {
   modulesHC_.reset();
 }
 
-void LSTEvent::addInputToEvent(LSTInputDeviceCollection const* lstInputDC) {
+void LSTEvent::addInputToEvent(LSTInputCollection const* lstInputDC) {
   lstInputDC_ = lstInputDC;
 
   pixelSize_ = lstInputDC_->sizes()[2];
