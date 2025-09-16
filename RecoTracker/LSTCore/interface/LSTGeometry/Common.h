043850f7a821d4a0e3b9180e35400efcc278d3bc
Andres Rios Tascon <ariostas@gmail.com>
2025-09-16 16:05:46 +0000

    Started implementing code in C++

---
+++ RecoTracker/LSTCore/interface/LSTGeometry/Common.h
@@ -0,0 +1,15 @@
+#ifndef RecoTracker_LSTCore_interface_LSTGeometry_Common_h
+#define RecoTracker_LSTCore_interface_LSTGeometry_Common_h
+
+#include <Eigen/Dense>
+
+namespace lst {
+
+  using MatrixD3x3 = Eigen::Matrix<double, 3, 3, Eigen::RowMajor>;
+  using MatrixD8x3 = Eigen::Matrix<double, 8, 3, Eigen::RowMajor>;
+  using ColVectorD3 = Eigen::Matrix<double, 3, 1>;
+  using RowVectorD3 = Eigen::Matrix<double, 1, 3>;
+
+}  // namespace lst
+
+#endif
---
+++ RecoTracker/LSTCore/src/LSTGeometry.cc
@@ -0,0 +1,4 @@
+// This is just a placeholder file to see if the code compiles
+//
+
+#include "RecoTracker/LSTCore/interface/LSTGeometry/CornerFunctions.h"
