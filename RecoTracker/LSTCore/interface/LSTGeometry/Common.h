e96fed5101a536e9bc68a84e7d79859c12c8a66b
Andres Rios Tascon <ariostas@gmail.com>
2025-09-17 16:59:02 -0400

    Added centroid methods

--- RecoTracker/LSTCore/interface/LSTGeometry/Common.h
+++ RecoTracker/LSTCore/interface/LSTGeometry/Common.h
@@ -2,6 +2,7 @@
 #define RecoTracker_LSTCore_interface_LSTGeometry_Common_h
 
 #include <Eigen/Dense>
+#include <limits>
 
 namespace lst {
 
@@ -10,6 +11,11 @@ namespace lst {
   using MatrixD8x3 = Eigen::Matrix<double, 8, 3, Eigen::RowMajor>;
   using ColVectorD3 = Eigen::Matrix<double, 3, 1>;
   using RowVectorD3 = Eigen::Matrix<double, 1, 3>;
+
+  // This is defined as a constant in case the legacy value (123456789) needs to be used
+  double kDefaultSlope = std::numeric_limits<double>::infinity();
+
+  double degToRad(double degrees) { return degrees * (std::numbers::pi_v<double> / 180); }
 
 }  // namespace lst
 
