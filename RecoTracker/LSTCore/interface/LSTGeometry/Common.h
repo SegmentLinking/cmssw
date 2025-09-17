332d4da4ba20b595111b808e6cfbec8351bde60c
Andres Rios Tascon <ariostas@gmail.com>
2025-09-17 14:17:16 -0400

    Finished with corners

--- RecoTracker/LSTCore/interface/LSTGeometry/Common.h
+++ RecoTracker/LSTCore/interface/LSTGeometry/Common.h
@@ -6,6 +6,7 @@
 namespace lst {
 
   using MatrixD3x3 = Eigen::Matrix<double, 3, 3, Eigen::RowMajor>;
+  using MatrixD4x3 = Eigen::Matrix<double, 4, 3, Eigen::RowMajor>;
   using MatrixD8x3 = Eigen::Matrix<double, 8, 3, Eigen::RowMajor>;
   using ColVectorD3 = Eigen::Matrix<double, 3, 1>;
   using RowVectorD3 = Eigen::Matrix<double, 1, 3>;
