01e5f7d7b23488b6c217c153a8d63d80ab522fda
Andres Rios Tascon <ariostas@gmail.com>
2025-09-17 16:59:02 -0400

    Added centroid methods

--- RecoTracker/LSTCore/interface/LSTGeometry/CentroidMethods.h
+++ RecoTracker/LSTCore/interface/LSTGeometry/CentroidMethods.h
@@ -1,4 +1,80 @@
 #ifndef RecoTracker_LSTCore_interface_LSTGeometry_CentroidMethods_h
 #define RecoTracker_LSTCore_interface_LSTGeometry_CentroidMethods_h
 
+#include <stdexcept>
+#include <unordered_map>
+
+#include "Common.h"
+#include "RecoTracker/LSTCore/interface/LSTGeometry/ModuleDetIdParser.h"
+#include "RecoTracker/LSTCore/interface/LSTGeometry/Centroid.h"
+#include "RecoTracker/LSTCore/interface/LSTGeometry/SensorInfo.h"
+
+namespace lst {
+
+  unsigned int extractBits(unsigned int value, unsigned int start, unsigned int end) {
+    unsigned int mask = (1 << (end - start + 1)) - 1;
+    return (value >> start) & mask;
+  }
+
+  unsigned int firstDigit(unsigned int n) {
+    while (n >= 10) {
+      n /= 10;
+    }
+    return n;
+  }
+
+  // TODO: refactor to use Module class better
+  int parseModuleType(unsigned int detId) {
+    // Check if the first digit of detId is '3' for inner tracker
+    if (firstDigit(detId) == 3)
+      return -1;
+
+    unsigned int subdet = extractBits(detId, 25, 27);
+    unsigned int layer = subdet == Module::SubDet::Barrel ? extractBits(detId, 20, 22) : extractBits(detId, 18, 20);
+    unsigned int ring = subdet == Module::SubDet::Endcap ? extractBits(detId, 12, 15) : 0;
+
+    bool is_even_det_id = detId % 2 == 0;
+    if (subdet == Module::SubDet::Barrel) {
+      if (layer <= 3)
+        return is_even_det_id ? Module::ModuleType::PSS : Module::ModuleType::PSP;
+      else
+        return Module::ModuleType::TwoS;
+    } else if (subdet == Module::SubDet::Endcap) {
+      if (layer <= 2)
+        return is_even_det_id && ring <= 10 ? Module::ModuleType::PSS
+                                            : (ring <= 10 ? Module::ModuleType::PSP : Module::ModuleType::TwoS);
+      else
+        return is_even_det_id && ring <= 7 ? Module::ModuleType::PSS
+                                           : (ring <= 7 ? Module::ModuleType::PSP : Module::ModuleType::TwoS);
+    } else {
+      throw std::runtime_error("Invalid subdetector type");
+    }
+  }
+
+  std::unordered_map<unsigned int, Centroid> compute_centroids(std::vector<SensorInfo> sensors) {
+    std::unordered_map<unsigned int, Centroid> centroids;
+    for (const auto& sensor : sensors) {
+      unsigned int detId = sensor.detId;
+      int moduleType = parseModuleType(detId);
+
+      // Remove sensors from inner tracker
+      if (moduleType == -1) {
+        continue;
+      }
+
+      // Convert from mm to cm
+      double z = sensor.sensorCenterZ_mm / 10.0;
+      double rho = sensor.sensorCenterRho_mm / 10.0;
+      double phi = degToRad(sensor.phi_deg);
+      double x = rho * cos(phi);
+      double y = rho * sin(phi);
+
+      Centroid centroid{detId, static_cast<unsigned int>(moduleType), x, y, z};
+      centroids[detId] = centroid;
+    }
+    return centroids;
+  }
+
+}  // namespace lst
+
 #endif
