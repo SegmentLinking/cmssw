3d1ec0e388e31271139bcba91d3f9c3e8bd9fe52
Andres Rios Tascon <ariostas@gmail.com>
2025-09-25 10:49:09 -0400

    Finished with pixel maps

--- RecoTracker/LSTCore/interface/LSTGeometry/DetectorGeometry.h
+++ RecoTracker/LSTCore/interface/LSTGeometry/DetectorGeometry.h
@@ -1,6 +1,7 @@
 #ifndef RecoTracker_LSTCore_interface_LSTGeometry_DetectorGeometry_h
 #define RecoTracker_LSTCore_interface_LSTGeometry_DetectorGeometry_h
 
+#include <algorithm>
 #include <vector>
 
 #include "RecoTracker/LSTCore/interface/LSTGeometry/Common.h"
@@ -9,26 +10,26 @@ namespace lst {
 
   class DetectorGeometry {
   private:
-    std::unordered_map<unsigned int, MatrixD4x3> corners;
-    std::vector<double> avg_radii;
-    std::vector<double> avg_z;
+    std::unordered_map<unsigned int, MatrixD4x3> corners_;
+    std::vector<double> avg_radii_;
+    std::vector<double> avg_z_;
 
   public:
-    DetectorGeometry(std::unordered_map<unsigned int, MatrixD4x3> corners_in,
-                     std::vector<double> avg_radii_in,
-                     std::vector<double> avg_z_in)
-        : corners(corners_in), avg_radii(avg_radii_in), avg_z(avg_z_in) {}
+    DetectorGeometry(std::unordered_map<unsigned int, MatrixD4x3> corners,
+                     std::vector<double> avg_radii,
+                     std::vector<double> avg_z)
+        : corners_(corners), avg_radii_(avg_radii), avg_z_(avg_z) {}
 
     std::vector<unsigned int> getDetIds() const {
       std::vector<unsigned int> detIds;
-      for (const auto& entry : corners) {
+      for (const auto& entry : corners_) {
         detIds.push_back(entry.first);
       }
       return detIds;
     }
 
     double getMinR(unsigned int detId) const {
-      auto const& corners = this->corners.at(detId);
+      auto const& corners = corners_.at(detId);
       double minR = std::numeric_limits<double>::max();
       for (int i = 0; i < corners.rows(); i++) {
         double x = corners(i, 1);
@@ -39,7 +40,7 @@ namespace lst {
     }
 
     double getMaxR(unsigned int detId) const {
-      auto const& corners = this->corners.at(detId);
+      auto const& corners = corners_.at(detId);
       double maxR = std::numeric_limits<double>::min();
       for (int i = 0; i < corners.rows(); i++) {
         double x = corners(i, 1);
@@ -47,6 +48,109 @@ namespace lst {
         maxR = std::max(maxR, std::sqrt(x * x + y * y));
       }
       return maxR;
+    }
+
+    double getMinz(unsigned int detId) const {
+      auto const& corners = corners_.at(detId);
+      double minZ = std::numeric_limits<double>::max();
+      for (int i = 0; i < corners.rows(); i++) {
+        double z = corners(i, 0);
+        minZ = std::min(minZ, z);
+      }
+      return minZ;
+    }
+
+    double getMaxz(unsigned int detId) const {
+      auto const& corners = corners_.at(detId);
+      double maxZ = std::numeric_limits<double>::min();
+      for (int i = 0; i < corners.rows(); i++) {
+        double z = corners(i, 0);
+        maxZ = std::max(maxZ, z);
+      }
+      return maxZ;
+    }
+
+    double getMinPhi(unsigned int detId) const {
+      auto const& corners = corners_.at(detId);
+      double minPhi = std::numeric_limits<double>::max();
+      double minPosPhi = std::numeric_limits<double>::max();
+      double minNegPhi = std::numeric_limits<double>::max();
+      unsigned int nPos = 0;
+      unsigned int nOverPi2 = 0;
+      for (int i = 0; i < corners.rows(); i++) {
+        double phi = phi_mpi_pi(std::numbers::pi_v<double> + std::atan2(-corners(i, 2), -corners(i, 1)));
+        minPhi = std::min(minPhi, phi);
+        if (phi > 0) {
+          minPosPhi = std::min(minPosPhi, phi);
+          nPos++;
+        } else {
+          minNegPhi = std::min(minNegPhi, phi);
+        }
+        if (std::fabs(phi) > std::numbers::pi_v<double> / 2.) {
+          nOverPi2++;
+        }
+      }
+      if (nPos == 4 || nPos == 0)
+        return minPhi;
+      if (nOverPi2 == 4)
+        return minPosPhi;
+      return minPhi;
+    }
+
+    double getMaxPhi(unsigned int detId) const {
+      auto const& corners = corners_.at(detId);
+      double maxPhi = std::numeric_limits<double>::min();
+      double maxPosPhi = std::numeric_limits<double>::min();
+      double maxNegPhi = std::numeric_limits<double>::min();
+      unsigned int nPos = 0;
+      unsigned int nOverPi2 = 0;
+      for (int i = 0; i < corners.rows(); i++) {
+        double phi = phi_mpi_pi(std::numbers::pi_v<double> + std::atan2(-corners(i, 2), -corners(i, 1)));
+        maxPhi = std::max(maxPhi, phi);
+        if (phi > 0) {
+          maxPosPhi = std::max(maxPosPhi, phi);
+          nPos++;
+        } else {
+          maxNegPhi = std::max(maxNegPhi, phi);
+        }
+        if (std::fabs(phi) > std::numbers::pi_v<double> / 2.) {
+          nOverPi2++;
+        }
+      }
+      if (nPos == 4 || nPos == 0)
+        return maxPhi;
+      if (nOverPi2 == 4)
+        return maxNegPhi;
+      return maxPhi;
+    }
+
+    std::pair<double, double> getCompatibleEtaRange(unsigned int detId, double zmin_bound, double zmax_bound) const {
+      double minr = getMinR(detId);
+      double maxr = getMaxR(detId);
+      double minz = getMinz(detId);
+      double maxz = getMaxz(detId);
+      double mineta = -std::log(std::tan(std::atan2(minz > 0 ? maxr : minr, minz - zmin_bound) / 2.));
+      double maxeta = -std::log(std::tan(std::atan2(maxz > 0 ? minr : maxr, maxz - zmax_bound) / 2.));
+
+      if (maxeta < mineta)
+        std::swap(maxeta, mineta);
+      return std::make_pair(mineta, maxeta);
+    }
+
+    std::pair<std::pair<double, double>, std::pair<double, double>> getCompatiblePhiRange(unsigned int detId,
+                                                                                          double ptmin,
+                                                                                          double ptmax) const {
+      double minr = getMinR(detId);
+      double maxr = getMaxR(detId);
+      double minphi = getMinPhi(detId);
+      double maxphi = getMaxPhi(detId);
+      double A = k2Rinv1GeVf * kB / 2.;
+      double pos_q_phi_lo_bound = phi_mpi_pi(A * minr / ptmax + minphi);
+      double pos_q_phi_hi_bound = phi_mpi_pi(A * maxr / ptmin + maxphi);
+      double neg_q_phi_lo_bound = phi_mpi_pi(-A * maxr / ptmin + minphi);
+      double neg_q_phi_hi_bound = phi_mpi_pi(-A * minr / ptmax + maxphi);
+      return std::make_pair(std::make_pair(pos_q_phi_lo_bound, pos_q_phi_hi_bound),
+                            std::make_pair(neg_q_phi_lo_bound, neg_q_phi_hi_bound));
     }
   };
 }  // namespace lst
--- RecoTracker/LSTCore/interface/LSTGeometry/PixelMapMethods.h
+++ RecoTracker/LSTCore/interface/LSTGeometry/PixelMapMethods.h
@@ -28,7 +28,7 @@ namespace lst {
     PtEtaPhiZChargeMap maps;
 
     // Initialize empty lists for the pixel map
-    for (unsigned int ipt = 0; ipt < sizeof(kPtBounds) / sizeof(kPtBounds[0]); ipt++) {
+    for (unsigned int ipt = 0; ipt < sizeof(kPtBounds) / sizeof(kPtBounds[0]) - 1; ipt++) {
       for (unsigned int ieta = 0; ieta < kNEta; ieta++) {
         for (unsigned int iphi = 0; iphi < kNPhi; iphi++) {
           for (unsigned int iz = 0; iz < kNZ; iz++) {
@@ -61,6 +61,66 @@ namespace lst {
       // Skip if the module is not PS module and is not lower module
       if (module.isLower() != 1 || module.moduleType() != 0)
         continue;
+
+      // For this module, now compute which super bins they belong to
+      // To compute which super bins it belongs to, one needs to provide at least pt and z window to compute compatible eta and phi range
+      // So we have a loop in pt and Z
+      for (unsigned int ipt = 0; ipt < sizeof(kPtBounds) / sizeof(kPtBounds[0]) - 1; ipt++) {
+        for (unsigned int iz = 0; iz < kNZ; iz++) {
+          // The zmin, zmax of consideration
+          double zmin = -30 + iz * (60. / kNZ);
+          double zmax = -30 + (iz + 1) * (60. / kNZ);
+
+          zmin -= 0.05;
+          zmin += 0.05;
+
+          // The ptmin, ptmax of consideration
+          double pt_lo = kPtBounds[ipt];
+          double pt_hi = kPtBounds[ipt + 1];
+
+          auto [etamin, etamax] = det_geom.getCompatibleEtaRange(detId, zmin, zmax);
+
+          etamin -= 0.05;
+          etamax += 0.05;
+
+          if (layer == 2 && subdet == 4) {
+            if (etamax < 2.3)
+              continue;
+            if (etamin < 2.3)
+              etamin = 2.3;
+          }
+
+          // Compute the indices of the compatible eta range
+          unsigned int ietamin = static_cast<unsigned int>(std::max((etamin + 2.6) / (5.2 / kNEta), 0.0));
+          unsigned int ietamax =
+              static_cast<unsigned int>(std::min((etamax + 2.6) / (5.2 / kNEta), static_cast<double>(kNEta)));
+
+          auto phi_ranges = det_geom.getCompatiblePhiRange(detId, pt_lo, pt_hi);
+
+          int iphimin_pos = static_cast<int>((phi_ranges.first.first + std::numbers::pi_v<double>) /
+                                             (2. * std::numbers::pi_v<double> / kNPhi));
+          int iphimax_pos = static_cast<int>((phi_ranges.first.second + std::numbers::pi_v<double>) /
+                                             (2. * std::numbers::pi_v<double> / kNPhi));
+          int iphimin_neg = static_cast<int>((phi_ranges.second.first + std::numbers::pi_v<double>) /
+                                             (2. * std::numbers::pi_v<double> / kNPhi));
+          int iphimax_neg = static_cast<int>((phi_ranges.second.second + std::numbers::pi_v<double>) /
+                                             (2. * std::numbers::pi_v<double> / kNPhi));
+
+          unsigned int phibins_pos_start = iphimin_pos <= iphimax_pos ? iphimin_pos : 0;
+          unsigned int phibins_pos_end = iphimin_pos <= iphimax_pos ? iphimax_pos : kNPhi;
+          unsigned int phibins_neg_start = iphimin_neg <= iphimax_neg ? iphimin_neg : 0;
+          unsigned int phibins_neg_end = iphimin_neg <= iphimax_neg ? iphimax_neg : kNPhi;
+
+          for (unsigned int ieta = ietamin; ieta <= ietamax; ieta++) {
+            for (unsigned int iphi = phibins_pos_start; iphi < phibins_pos_end; iphi++) {
+              maps[{ipt, ieta, iphi, iz, 1}][{layer, subdet}].push_back(detId);
+            }
+            for (unsigned int iphi = phibins_neg_start; iphi < phibins_neg_end; iphi++) {
+              maps[{ipt, ieta, iphi, iz, 1}][{layer, subdet}].push_back(detId);
+            }
+          }
+        }
+      }
     }
 
     return maps;
