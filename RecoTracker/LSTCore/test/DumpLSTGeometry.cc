08051cad79cffcc975f3da1b24f33d8750259977
Andres Rios Tascon <ariostas@gmail.com>
2025-11-19 18:32:55 +0000

    Some progress getting required data

--- RecoTracker/LSTCore/test/DumpLSTGeometry.cc
+++ RecoTracker/LSTCore/test/DumpLSTGeometry.cc
@@ -6,19 +6,31 @@
 
 #include "RecoTracker/Record/interface/TrackerRecoGeometryRecord.h"
 
+// temporary
+#include <string>
+#include "FWCore/Utilities/interface/typelookup.h"
+TYPELOOKUP_DATA_REG(std::string);
+// end temporary
+
 class DumpLSTGeometry : public edm::one::EDAnalyzer<> {
 public:
   explicit DumpLSTGeometry(const edm::ParameterSet& config);
 
 private:
   void analyze(const edm::Event& event, const edm::EventSetup& eventSetup) override;
+  
+  edm::ESGetToken<std::string, TrackerRecoGeometryRecord> lstGeoToken_;
 
   std::string outputDirectory_;
 };
 
 DumpLSTGeometry::DumpLSTGeometry(const edm::ParameterSet& config)
-    : outputDirectory_(config.getUntrackedParameter<std::string>("outputDirectory", "data")) {}
+    : lstGeoToken_{esConsumes()}, outputDirectory_(config.getUntrackedParameter<std::string>("outputDirectory", "data")) {}
 
-void DumpLSTGeometry::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) {}
+void DumpLSTGeometry::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
+    const auto& lstg = iSetup.getData(lstGeoToken_);
+    
+    edm::LogInfo("DumpLSTGeometry") << lstg << std::endl;
+}
 
 DEFINE_FWK_MODULE(DumpLSTGeometry);
