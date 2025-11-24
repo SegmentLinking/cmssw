b8dc8ff856a95507136ce6fd463af452e553724d
Andres Rios Tascon <ariostas@gmail.com>
2025-11-24 21:45:34 +0000

    All CMSSW inputs work

--- RecoTracker/LSTCore/plugins/LSTGeometryESProducer.cc
+++ RecoTracker/LSTCore/plugins/LSTGeometryESProducer.cc
@@ -18,6 +18,7 @@
 
 #include "RecoTracker/LSTCore/interface/LSTGeometry/SensorInfo.h"
 #include "RecoTracker/LSTCore/interface/LSTGeometry/ModuleInfo.h"
+#include "RecoTracker/LSTCore/interface/LSTGeometry/Module.h"
 
 #include <cmath>
 #include <vector>
@@ -62,125 +63,109 @@ void LSTGeometryESProducer::fillDescriptions(edm::ConfigurationDescriptions &des
 std::unique_ptr<std::string> LSTGeometryESProducer::produce(const TrackerRecoGeometryRecord &iRecord) {
   trackerGeom_ = &iRecord.get(geomToken_);
   trackerTopo_ = &iRecord.get(ttopoToken_);
-  
+
   std::vector<lstgeometry::SensorInfo> sensors;
   std::vector<lstgeometry::ModuleInfo> modules;
-  
+
+  std::vector<float> avg_r_cm(6, 0.0);
+  std::vector<float> avg_z_cm(5, 0.0);
+  std::vector<unsigned int> avg_r_counter(6, 0);
+  std::vector<unsigned int> avg_z_counter(5, 0);
+
   for (auto &det : trackerGeom_->dets()) {
-      
-      const DetId detId = det->geographicalId();
-      
-      const auto &surface = det->surface();
-      const Bounds *b = &(surface).bounds();
-      const auto &position = surface.position();
-      
-      double rho_cm = position.perp();
-      double z_cm = position.z();
-      const double phi_rad = position.phi();
-      
-      if (det->isLeaf()) {
-          // Leafs are the sensors 
-            lstgeometry::SensorInfo sensor;
-            sensor.detId = detId();
-            sensor.sensorCenterRho_cm = rho_cm;
-            sensor.sensorCenterZ_cm = z_cm;
-            sensor.phi_rad = phi_rad;
-            sensors.push_back(sensor);
-            continue;
-      }
-      
-      double tiltAngle_rad = std::asin(det->rotation().zz());
-      
-      double meanWidth_cm = 0.0;
-      double length_cm = 0.0;
-      
-      if (det->components().size() != 2) {
-          std::cout << "Not equal to 2! " << det->components().size() << "\n";
-          continue;
-      }
-      
-      double sensorSpacing_cm = det->components()[0]->toLocal(det->components()[1]->position()).mag() * 10;
-      
-      // if (detId() != 438043652)
-      //     continue;
-    
-      // std::cout << "DetId " << detId() << "\n";
-      // std::cout << "rho_cm " << rho_cm << "\n";
-      // std::cout << "z_cm " << z_cm << "\n";
-      // std::cout << "det " << detId.det() << "\n";
-      // std::cout << "subdet " << detId.subdetId() << "\n";
-      // std::cout << "layer " << trackerTopo_->layer(detId) << "\n";
-      // std::cout << "print " << trackerTopo_->print(detId) << "\n";
-      
-      double vtxOneX_cm, vtxOneY_cm, vtxTwoX_cm, vtxTwoY_cm, vtxThreeX_cm, vtxThreeY_cm, vtxFourX_cm, vtxFourY_cm;
-      double vtxOneX_cm_tmp, vtxOneY_cm_tmp, vtxTwoX_cm_tmp, vtxTwoY_cm_tmp, vtxThreeX_cm_tmp, vtxThreeY_cm_tmp, vtxFourX_cm_tmp, vtxFourY_cm_tmp;
-      
-      if (GeomDetEnumerators::isBarrel(det->subDetector())) {
-          if (const RectangularPlaneBounds *b2 = dynamic_cast<const RectangularPlaneBounds *>(b)) {
-              // Rectangular
-              meanWidth_cm = b2->width();
-              length_cm = b2->length();
-              float dx = b2->width() * 0.5;   // half width
-              float dy = b2->length() * 0.5;  // half length
-              // std::cout << "Rectangle parameters " << b2->width() << " " << b2->length() << "\n";
-              // std::cout << std::sin(phi_rad) << "\n";
-              // vtxOneX_cm = rho_cm * cos(phi_rad)  -dx * sin(phi_rad);
-              // vtxOneY_cm = rho_cm * sin(phi_rad) -dy * cos(phi_rad);
-              // vtxTwoX_cm = rho_cm * cos(phi_rad) -dx * sin(phi_rad);
-              // vtxTwoY_cm = rho_cm * sin(phi_rad) + dy * cos(phi_rad);
-              // vtxThreeX_cm = rho_cm * cos(phi_rad) + dx * sin(phi_rad);
-              // vtxThreeY_cm = rho_cm * sin(phi_rad) + dy * cos(phi_rad);
-              // vtxFourX_cm = rho_cm * cos(phi_rad) + dx * sin(phi_rad);
-              // vtxFourY_cm = rho_cm * sin(phi_rad) -dy * cos(phi_rad);
-              vtxOneX_cm_tmp = rho_cm - dy * std::sin(tiltAngle_rad);
-              vtxOneY_cm_tmp = dx;
-              vtxTwoX_cm_tmp = rho_cm + dy * std::sin(tiltAngle_rad);
-              vtxTwoY_cm_tmp = dx;
-              vtxThreeX_cm_tmp = rho_cm - dy * std::sin(tiltAngle_rad);
-              vtxThreeY_cm_tmp = -dx;
-              vtxFourX_cm_tmp = rho_cm + dy * std::sin(tiltAngle_rad);
-              vtxFourY_cm_tmp = -dx;
-            } else {
-              throw cms::Exception("UnimplementedFeature") << "unsupported Bounds class";
-            }
-          // rho_cm -= sensorSpacing_cm/2.0;
-      } else {
-          if (const RectangularPlaneBounds *b2 = dynamic_cast<const RectangularPlaneBounds *>(b)) {
-              // Rectangular
-              meanWidth_cm = b2->width();
-              length_cm = b2->length();
-              float dx = b2->width() * 0.5;   // half width
-              float dy = b2->length() * 0.5;  // half length
-              // std::cout << "Rectangle parameters " << b2->width() << " " << b2->length() << "\n";
-              vtxOneX_cm_tmp = rho_cm + dy;
-              vtxOneY_cm_tmp = dx;
-              vtxTwoX_cm_tmp = rho_cm - dy;
-              vtxTwoY_cm_tmp = dx;
-              vtxThreeX_cm_tmp = rho_cm - dy;
-              vtxThreeY_cm_tmp = -dx;
-              vtxFourX_cm_tmp = rho_cm + dy;
-              vtxFourY_cm_tmp = -dx;
-              std::cout << detId() << " " << vtxOneX_cm_tmp << " " << vtxOneY_cm_tmp << " " << vtxTwoX_cm_tmp << " " << vtxTwoY_cm_tmp << " " << vtxThreeX_cm_tmp << " " << vtxThreeY_cm_tmp << " " << vtxFourX_cm_tmp << " " << vtxFourY_cm_tmp << std::endl;
-            } else {
-              throw cms::Exception("UnimplementedFeature") << "unsupported Bounds class";
-            }
-          
-          // z_cm -= sensorSpacing_cm/2.0; // Not sure why
-      }
-      
-      vtxOneX_cm = vtxOneX_cm_tmp * cos(phi_rad) + vtxOneY_cm_tmp * sin(phi_rad);
-      vtxOneY_cm = vtxOneX_cm_tmp * sin(phi_rad) - vtxOneY_cm_tmp * cos(phi_rad);
-      vtxTwoX_cm = vtxTwoX_cm_tmp * cos(phi_rad) + vtxTwoY_cm_tmp * sin(phi_rad);
-      vtxTwoY_cm = vtxTwoX_cm_tmp * sin(phi_rad) - vtxTwoY_cm_tmp * cos(phi_rad);
-      vtxThreeX_cm = vtxThreeX_cm_tmp * cos(phi_rad) + vtxThreeY_cm_tmp * sin(phi_rad);
-      vtxThreeY_cm = vtxThreeX_cm_tmp * sin(phi_rad) - vtxThreeY_cm_tmp * cos(phi_rad);
-      vtxFourX_cm = vtxFourX_cm_tmp * cos(phi_rad) + vtxFourY_cm_tmp * sin(phi_rad);
-      vtxFourY_cm = vtxFourX_cm_tmp * sin(phi_rad) - vtxFourY_cm_tmp * cos(phi_rad);
-      
-       std::cout << detId() << " " << rho_cm << " " << z_cm << " " << phi_rad << " " << vtxOneX_cm << " " << vtxOneY_cm << " " << vtxTwoX_cm << " " << vtxTwoY_cm << " " << vtxThreeX_cm << " " << vtxThreeY_cm << " " << vtxFourX_cm << " " << vtxFourY_cm << " " << meanWidth_cm << " " << length_cm << " " << sensorSpacing_cm << " " << tiltAngle_rad << std::endl;
+    const DetId detId = det->geographicalId();
+
+    const auto &surface = det->surface();
+    const Bounds *b = &(surface).bounds();
+    const auto &position = surface.position();
+
+    double rho_cm = position.perp();
+    double z_cm = position.z();
+    const double phi_rad = position.phi();
+
+    if (det->isLeaf()) {
+      // Leafs are the sensors
+      lstgeometry::SensorInfo sensor;
+      sensor.detId = detId();
+      sensor.sensorCenterRho_cm = rho_cm;
+      sensor.sensorCenterZ_cm = z_cm;
+      sensor.phi_rad = phi_rad;
+      sensors.push_back(sensor);
+      continue;
+    }
+
+    const RectangularPlaneBounds *b2 = dynamic_cast<const RectangularPlaneBounds *>(b);
+    if (!b2) {
+      throw cms::Exception("UnimplementedFeature") << "unsupported Bounds class";
+    }
+
+    double tiltAngle_rad = std::asin(det->rotation().zz());
+
+    double meanWidth_cm = b2->width();
+    double length_cm = b2->length();
+    double dx = b2->width() * 0.5;
+    double dy = b2->length() * 0.5;
+
+    double sensorSpacing_cm = det->components()[0]->toLocal(det->components()[1]->position()).mag();
+
+    double vtxOneX_cm_tmp = rho_cm - dy * std::sin(tiltAngle_rad);
+    double vtxOneY_cm_tmp = dx;
+    double vtxTwoX_cm_tmp = rho_cm + dy * std::sin(tiltAngle_rad);
+    double vtxTwoY_cm_tmp = dx;
+    double vtxThreeX_cm_tmp = rho_cm - dy * std::sin(tiltAngle_rad);
+    double vtxThreeY_cm_tmp = -dx;
+    double vtxFourX_cm_tmp = rho_cm + dy * std::sin(tiltAngle_rad);
+    double vtxFourY_cm_tmp = -dx;
+
+    double vtxOneX_cm = vtxOneX_cm_tmp * cos(phi_rad) + vtxOneY_cm_tmp * sin(phi_rad);
+    double vtxOneY_cm = vtxOneX_cm_tmp * sin(phi_rad) - vtxOneY_cm_tmp * cos(phi_rad);
+    double vtxTwoX_cm = vtxTwoX_cm_tmp * cos(phi_rad) + vtxTwoY_cm_tmp * sin(phi_rad);
+    double vtxTwoY_cm = vtxTwoX_cm_tmp * sin(phi_rad) - vtxTwoY_cm_tmp * cos(phi_rad);
+    double vtxThreeX_cm = vtxThreeX_cm_tmp * cos(phi_rad) + vtxThreeY_cm_tmp * sin(phi_rad);
+    double vtxThreeY_cm = vtxThreeX_cm_tmp * sin(phi_rad) - vtxThreeY_cm_tmp * cos(phi_rad);
+    double vtxFourX_cm = vtxFourX_cm_tmp * cos(phi_rad) + vtxFourY_cm_tmp * sin(phi_rad);
+    double vtxFourY_cm = vtxFourX_cm_tmp * sin(phi_rad) - vtxFourY_cm_tmp * cos(phi_rad);
+
+    unsigned int detid = detId();
+
+    unsigned short layer = lstgeometry::Module::parseLayer(detid);
+    if (lstgeometry::Module::parseSubdet(detid) == lstgeometry::Module::SubDet::Barrel) {
+      avg_r_cm[layer - 1] += rho_cm;
+      avg_r_counter[layer - 1] += 1;
+    } else {
+      avg_z_cm[layer - 1] += std::fabs(z_cm);
+      avg_z_counter[layer - 1] += 1;
+    }
+
+    lstgeometry::ModuleInfo module;
+    module.detId = detid;
+    module.sensorCenterRho_cm = rho_cm;
+    module.sensorCenterZ_cm = z_cm;
+    module.tiltAngle_rad = tiltAngle_rad;
+    module.skewAngle_rad = 0.0;
+    module.yawAngle_rad = 0.0;
+    module.phi_rad = phi_rad;
+    module.vtxOneX_cm = vtxOneX_cm;
+    module.vtxOneY_cm = vtxOneY_cm;
+    module.vtxTwoX_cm = vtxTwoX_cm;
+    module.vtxTwoY_cm = vtxTwoY_cm;
+    module.vtxThreeX_cm = vtxThreeX_cm;
+    module.vtxThreeY_cm = vtxThreeY_cm;
+    module.vtxFourX_cm = vtxFourX_cm;
+    module.vtxFourY_cm = vtxFourY_cm;
+    module.meanWidth_cm = meanWidth_cm;
+    module.length_cm = length_cm;
+    module.sensorSpacing_cm = sensorSpacing_cm;
+    modules.push_back(module);
   }
 
-  // placeholder
+  for (size_t i = 0; i < avg_r_cm.size(); ++i) {
+    avg_r_cm[i] /= avg_r_counter[i];
+  }
+  for (size_t i = 0; i < avg_z_cm.size(); ++i) {
+    avg_z_cm[i] /= avg_z_counter[i];
+  }
+
   return std::make_unique<std::string>("LSTGeometryESProducer");
 }
 
