6fb00158d2bbfeed8ff91b3b1375efa2bcfa6e49
Andres Rios Tascon <ariostas@gmail.com>
2025-12-17 15:33:57 -0500

    Didn't actually need corner coordinates

--- RecoTracker/LSTCore/interface/LSTGeometry/ModuleInfo.h
+++ RecoTracker/LSTCore/interface/LSTGeometry/ModuleInfo.h
@@ -13,14 +13,6 @@ namespace lstgeometry {
     double skewAngle_rad;
     double yawAngle_rad;
     double phi_rad;
-    double vtxOneX_cm;
-    double vtxOneY_cm;
-    double vtxTwoX_cm;
-    double vtxTwoY_cm;
-    double vtxThreeX_cm;
-    double vtxThreeY_cm;
-    double vtxFourX_cm;
-    double vtxFourY_cm;
     double meanWidth_cm;
     double length_cm;
     double sensorSpacing_cm;
