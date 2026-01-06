69834072a572ed17d4a89515edc31c08a8bc6343
Andres Rios Tascon <ariostas@gmail.com>
2026-01-06 17:56:05 +0000

    Added cli options to test

--- RecoTracker/LSTCore/test/dumpLSTGeometry.py
+++ RecoTracker/LSTCore/test/dumpLSTGeometry.py
@@ -1,4 +1,7 @@
 import FWCore.ParameterSet.Config as cms
+from Configuration.AlCa.GlobalTag import GlobalTag
+import Configuration.Geometry.defaultPhase2ConditionsEra_cff as _settings
+import FWCore.ParameterSet.VarParsing as VarParsing
 
 trackingLSTCommon = cms.Modifier()
 trackingLST = cms.ModifierChain(trackingLSTCommon)
@@ -6,22 +9,20 @@ trackingLST = cms.ModifierChain(trackingLSTCommon)
 ###################################################################
 # Set default phase-2 settings
 ###################################################################
-import Configuration.Geometry.defaultPhase2ConditionsEra_cff as _settings
 _PH2_GLOBAL_TAG, _PH2_ERA = _settings.get_era_and_conditions(_settings.DEFAULT_VERSION)
 
 # No era in Fireworks/Geom reco dumper
-process = cms.Process('DUMP', _PH2_ERA, trackingLST)
+process = cms.Process("DUMP", _PH2_ERA, trackingLST)
 
 # import of standard configurations
-process.load('Configuration.StandardSequences.Services_cff')
-process.load('FWCore.MessageService.MessageLogger_cfi')
-process.load('Configuration.Geometry.GeometryExtendedRun4DefaultReco_cff')
-process.load('Configuration.StandardSequences.MagneticField_cff')
-process.load('Configuration.StandardSequences.Reconstruction_cff')
-process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.load("Configuration.StandardSequences.Services_cff")
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.load("Configuration.Geometry.GeometryExtendedRun4DefaultReco_cff")
+process.load("Configuration.StandardSequences.MagneticField_cff")
+process.load("Configuration.StandardSequences.Reconstruction_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 
-from Configuration.AlCa.GlobalTag import GlobalTag
-process.GlobalTag = GlobalTag(process.GlobalTag, _PH2_GLOBAL_TAG, '')
+process.GlobalTag = GlobalTag(process.GlobalTag, _PH2_GLOBAL_TAG, "")
 
 process.MessageLogger.cerr.threshold = "INFO"
 process.MessageLogger.cerr.LSTGeometryESProducer = dict(limit=-1)
@@ -29,18 +30,39 @@ process.MessageLogger.cerr.LSTGeometryESProducer = dict(limit=-1)
 process.source = cms.Source("EmptySource")
 process.maxEvents.input = 1
 
-
 process.add_(cms.ESProducer("LSTGeometryESProducer"))
 
-defaultOutputDirectory="data/"
-defaultPtCut="0.8"
-defaultToBinary=True
+options = VarParsing.VarParsing()
+options.register(
+    "outputDirectory",
+    "data/",
+    VarParsing.VarParsing.multiplicity.singleton,
+    VarParsing.VarParsing.varType.string,
+    "Output directory for LST geometry files",
+)
+options.register(
+    "ptCut",
+    0.8,
+    VarParsing.VarParsing.multiplicity.singleton,
+    VarParsing.VarParsing.varType.float,
+    "pT cut for LST module maps",
+)
+options.register(
+    "binaryOutput",
+    True,
+    VarParsing.VarParsing.multiplicity.singleton,
+    VarParsing.VarParsing.varType.bool,
+    "Dump LST geometry as binary files",
+)
+options.parseArguments()
 
-process.dump = cms.EDAnalyzer("DumpLSTGeometry",
-                              outputDirectory = cms.untracked.string(defaultOutputDirectory),
-                              ptCut = cms.string(defaultPtCut),
-                              outputAsBinary = cms.untracked.bool(defaultToBinary),
-                              )
 
-print("Requesting LST geometry dump into directory:", defaultOutputDirectory, "\n");
+process.dump = cms.EDAnalyzer(
+    "DumpLSTGeometry",
+    outputDirectory = cms.untracked.string(options.outputDirectory),
+    ptCut = cms.string(str(options.ptCut)),
+    outputAsBinary = cms.untracked.bool(options.binaryOutput),
+)
+
+print(f"Requesting LST geometry dump into directory: {options.outputDirectory}\n")
 process.p = cms.Path(process.dump)
