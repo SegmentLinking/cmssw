936cbb5d72f9fb9054cc74326174da11fe761e5f
Manos Vourliotis <emmanouil.vourliotis@gmail.com>
2026-07-29 02:32:08 -0700

    Fix nOTHits counting

--- RecoTracker/LST/plugins/LSTOutputConverter.cc
+++ RecoTracker/LST/plugins/LSTOutputConverter.cc
@@ -225,6 +225,7 @@ void LSTOutputConverter::produce(edm::Event& iEvent, const edm::EventSetup& iSet
     }
 
     if (iType != lst::LSTObjType::pLS) {
+      const int nPixelHits = static_cast<int>(recHits.size());
       // The pixel hits are packed into first kPixelLayerSlots layer slots.
       for (unsigned int layerSlot = lst::Params_TC::kPixelLayerSlots; layerSlot < lst::Params_TC::kLayers;
            ++layerSlot) {
@@ -307,7 +308,7 @@ void LSTOutputConverter::produce(edm::Event& iEvent, const edm::EventSetup& iSet
           const math::XYZPoint refPoint(-tip * sp, tip * cp_phi, zip);
           const math::XYZVector mom(pt_fit * cp_phi, pt_fit * sp, pt_fit * slope);
 
-          const int nOTHits = static_cast<int>(recHits.size());
+          const int nOTHits = static_cast<int>(recHits.size()) - nPixelHits;
           const double ndof = static_cast<double>(std::max(1, 2 * nOTHits - 5));
           const double chi2total = static_cast<double>(chi2stored) * ndof;
 
