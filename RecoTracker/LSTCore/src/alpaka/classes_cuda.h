1da3ee3f02a220a290a3d6fbad53389c151ea258
Manos Vourliotis <emmanouil.vourliotis@gmail.com>
2026-09-16 04:14:41 -0700

    Add the BLF track-candidate device collection to the CUDA and ROCm dictionaries

--- RecoTracker/LSTCore/src/alpaka/classes_cuda.h
+++ RecoTracker/LSTCore/src/alpaka/classes_cuda.h
@@ -4,6 +4,7 @@
 #include "DataFormats/Common/interface/DeviceProduct.h"
 #include "DataFormats/Common/interface/Wrapper.h"
 #include "RecoTracker/LSTCore/interface/alpaka/LSTInputDeviceCollection.h"
+#include "RecoTracker/LSTCore/interface/alpaka/TrackCandidatesBLFFitDeviceCollection.h"
 #include "RecoTracker/LSTCore/interface/alpaka/TrackCandidatesDeviceCollection.h"
 
 #endif
